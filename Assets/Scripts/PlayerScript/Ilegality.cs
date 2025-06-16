c44d4caf954c064562b7ee3c6c47643792e36482
mierka_m <162184787+marsborox@users.noreply.github.com>
2025-06-16 23:26:21 +0200

    trying to refactor the damn switch  in gamemanager

--- Assets/Scripts/PlayerScript/Ilegality.cs
+++ Assets/Scripts/PlayerScript/Ilegality.cs
@@ -1,16 +1,6 @@
 using UnityEngine;
 
-public class Ilegality : MonoBehaviour
+public class Ilegality : PlayerStatNew
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    
 }
--- Assets/Scripts/PlayerScript/PlayerStatNew.cs
+++ Assets/Scripts/PlayerScript/PlayerStatNew.cs
@@ -1,12 +1,39 @@
 using UnityEngine;
+using System;
+using UnityEngine.UI;
 
 public class PlayerStatNew : MonoBehaviour
 {
+    [Header("BaseStats")]
     public float statTimer;
-    public float statTimeElapsed;
     public int statTreshold;
     public int statAmmount;
     public int statIncrement;
+
+    [Header("Trash")]
+    public float trashTimer;
+    public int trashIncrementInterval;
+    public int trashAmmount;
+    public int trashIncrementAmmount;
+
+    [Header("TrashIncrement")]
+    public float trashIncrementTimer;
+    public int trashIncrementTimerInterval;
+    public int trashIncrementAmmount_____;
+    public int trashIncrement_increment;
+
+    [Header("IncomeFromFollowers")]
+    public float followerIncomeTimer;
+    public int followerIncomeTimerInterval;
+    public int followerIncomeAmmount;
+    public int followerAmmount;
+
+    [Header("Illegality")]
+    public float illegalityTimer;
+    public int illegalityTimerInterval;
+    public int illegalityAmmount;
+    public int illegalityIncrementAmmount;
+
 
     public void Start()
     {
@@ -22,21 +49,57 @@ public class PlayerStatNew : MonoBehaviour
     }
     void StatAddTimerV2()
     {
-        StatAddTimer(ref statTimeElapsed,statTreshold,ref statAmmount,statIncrement);
+        //StatAddTimer(ref statTimer,statTreshold,ref statAmmount,statIncrement);
+
     }
-    void StatAddTimer(ref float timeElapsed,int treshold,ref int ammount,int increment)
+    void StatAddTimer()
     {
-        statTimeElapsed +=TimeController.instance.elapsedDeltaTime;
+        statTimer += TimeController.instance.elapsedDeltaTime;
+        if (statTimer >= statTreshold)
+        {
+            //DoSound
+            //display floatingText
+            statTimer -= statTreshold;
+            //ChangeStat(statAmmount, statIncrement);
+        }
+    }
+    void StatAddTimer(ref float timeElapsed,int treshold,ref int ammount,int increment,Action method)
+    {
+        statTimer +=TimeController.instance.elapsedDeltaTime;
         if (timeElapsed >= treshold)
         {
             //DoSound
             //display floatingText
             timeElapsed -= treshold;
-            ChangeStat(ammount, increment);
+            //method();
         }
     }
-    public virtual void ChangeStat(int i1, int i2) 
+
+    void TestTEst()
+    {
+        TEstDamn(AddStats);
+    }
+
+    void TEstDamn(System.Action action)
+    {
+        //action;
+        action();
+    }
+    void ChangeTrash()
     { 
         
     }
+    void ChangeTrashIncrement()
+    { 
+    
+    }
+    void ChangeFollowerIncome()
+    { 
+    
+    }
+    void ChangeIllegality()
+    { 
+    
+    }
+
 }
